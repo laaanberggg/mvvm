4268d8617637aee0e6e2de119ce1316323460b72
laaanberggg <84227247+laaanberggg@users.noreply.github.com>
2022-02-21 05:05:58 +0300

    Add files via upload

---
+++ pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace murzaev_mvvm
+{
+    class pattern
+    {
+        public static int one = 0;
+        public static int two = 0;
+        public static int itog = 0;
+        public static List<string> operList = new List<string> { "+", "-", "*", "/" };
+        public static int operId = 0;
+    }
+}
---
+++ viewpattern.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace murzaev_mvvm
+{
+    class viewpattern:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string rezultChanged
+        {
+            get
+            {
+                return pattern.itog.ToString();
+            }
+        }
+        public List<string> operCombo
+        {
+            get
+            {
+                return pattern.operList;
+            }
+        }
+        public int operInd
+        {
+            set
+            {
+                pattern.operId = value;
+            }
+        }
+        public static string tbOne
+        {
+            set
+            {
+                pattern.one = Convert.ToInt32(value);
+            }
+        }
+        public static string tbTwo
+        {
+            set
+            {
+                pattern.two = Convert.ToInt32(value);
+            }
+        }
+        public RoutedCommand buttRez { get; set; } = new RoutedCommand();
+        public void Command_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            switch (pattern.operId)
+            {
+                case 0:
+                    pattern.itog = pattern.one + pattern.two;
+                    break;
+                case 1:
+                    pattern.itog = pattern.one - pattern.two;
+                    break;
+                case 2:
+                    pattern.itog = pattern.one * pattern.two;
+                    break;
+                case 3:
+                    pattern.itog = pattern.one / pattern.two;
+                    break;
+                default:
+                    pattern.itog = 404;
+                    break;
+            }
+            PropertyChanged(this, new PropertyChangedEventArgs("rezultChanged"));
+        }
+        public CommandBinding bind;
+        public viewpattern()
+        {
+            bind = new CommandBinding(buttRez);
+            bind.Executed += Command_Executed;
+        }
+    }
+}
+
